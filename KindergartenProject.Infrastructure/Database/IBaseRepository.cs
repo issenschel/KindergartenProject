06723abd54b59706d4eb01087ef805e9fa3a13d3
heaven-wanderer <luckystqx@gmail.com>
2023-11-22 23:31:28 +0300

    КП5 (подготовление) -Добавил биндинги для DatGrid -Добавил Database(публичные классы сущностей и интерфейс) -Создал зависимость от проекта Infrastructure -Установил для основного проекта с окнами пакеты EntityFramework и System.Data.SQLite

---
+++ KindergartenProject.Infrastructure/Database/IBaseRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KindergartenProject.Infrastructure.Database
+{
+    internal interface IBaseRepository<TEntity>
+    {
+        TEntity GetById(long id);
+        List<TEntity> GetList();
+    }
+}
